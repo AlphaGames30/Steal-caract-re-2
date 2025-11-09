3c278d610055cbb970d6cb878d3ca17f1639119b
AlphaGames30 <edonis.imbert@gmail.com>
2025-11-09 18:00:17 +0100

    Update supabase.js

--- server/services/supabase.js
+++ server/services/supabase.js
@@ -7,3 +7,6 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+const playset = Array.from({ length: 8 }, () => Array(12).fill(null));
+
