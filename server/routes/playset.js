3c787e23b066ae288dbb45668f4835fec6cff86f
AlphaGames30 <edonis.imbert@gmail.com>
2025-11-09 19:32:42 +0100

    Update playset.js

--- server/routes/playset.js
+++ server/routes/playset.js
@@ -1,13 +1,10 @@
-im// server/routes/playset.js
+// server/routes/playset.js
 import express from "express";
 import { createPlayset } from "../services/playset.js";
 
 const router = express.Router();
-
-// Crée un playset vide avec 8 bases x 12 slots
 let playset = createPlayset();
 
-// Route GET pour récupérer le playset
 router.get("/", (req, res) => {
   res.json(playset);
 });
