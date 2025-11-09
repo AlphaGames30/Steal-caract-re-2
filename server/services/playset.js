8d86cb5cd26b79998808052ba1481f33d8ce64ec
AlphaGames30 <edonis.imbert@gmail.com>
2025-11-09 18:13:28 +0100

    Update playset.js

--- server/services/playset.js
+++ server/services/playset.js
@@ -1,1 +1,4 @@
-const playset = Array.from({ length: 8 }, () => Array(12).fill(null));
+// server/services/playset.js
+export const createPlayset = () => {
+  return Array.from({ length: 8 }, () => Array(12).fill(null));
+};
