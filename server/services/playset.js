7724759be966b2ce82a244eb6a5e5b8770ae26c1
AlphaGames30 <edonis.imbert@gmail.com>
2025-11-09 18:54:10 +0100

    Update playset.js

--- server/services/playset.js
+++ server/services/playset.js
@@ -2,3 +2,16 @@
 export const createPlayset = () => {
   return Array.from({ length: 8 }, () => Array(12).fill(null));
 };
+
+// server/routes/playset.js
+import express from "express";
+import { createPlayset } from "../services/playset.js";
+
+const router = express.Router();
+let playset = createPlayset();
+
+router.get("/", (req, res) => {
+  res.json(playset);
+});
+
+export default router;
